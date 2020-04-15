b407452c9229d0d784022b2e17f13d6ca1040e55
seodle <sunny.avry@gmail.com>
2020-04-15 18:29:03 +0200

    first commit

---
+++ microphone.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from vosk import Model, KaldiRecognizer
+import os
+
+if not os.path.exists("model-fr"):
+    print ("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases and unpack as 'model-en' in the current folder.")
+    exit (1)
+
+import pyaudio
+import pyautogui
+import keyboard
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+model = Model("model-fr")
+rec = KaldiRecognizer(model, 16000)
+
+while True:
+    data = stream.read(2000)
+    print len(data)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
